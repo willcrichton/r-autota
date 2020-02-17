83a74104106743044cc601331d7b62ef618fbc3e
Will Crichton <wcrichto@cs.stanford.edu>
2020-02-16 17:25:58 -0800

    R studio talks to webpage through socket

--- R/autota.R
+++ R/autota.R
@@ -8,29 +8,48 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-library(shiny)
-library(miniUI)
+library(glue)
 
-autoTAAddin <- function() {
-  context <- rstudioapi::getActiveDocumentContext()
-  ui <- miniPage(
-    gadgetTitleBar('Test'),
-    miniContentPanel(
-      p("hello"),
-      uiOutput('output')
-    )
-  )
+DEBUG <- TRUE
 
-  server <- function(input, output, session) {
-    output$output <- renderUI({p("hi")})
-
-    observeEvent(input$done, {
-      stopApp()
-    })
+debug_cat <- function(...) {
+  if (DEBUG) {
+    cat(...);
   }
-
-  viewer <- paneViewer(300)
-  runGadget(ui, server, viewer = viewer)
 }
 
-autoTAAddin()
+autoTAAddin <- function(port=3000) {
+  editor <- rstudioapi::getSourceEditorContext()
+
+  viewer <- getOption("viewer")
+  viewer(glue("http://localhost:{port}"))
+
+  httpuv::stopAllServers()
+  s <- httpuv::startServer(
+    "127.0.0.1", 8080,
+    list(
+       onWSOpen = function(ws) {
+         # The ws object is a WebSocket object
+         debug_cat("Server connection opened.\n")
+
+         ws$onMessage(function(binary, message) {
+           debug_cat("Server received message:", message, "\n")
+          })
+         ws$onClose(function() {
+           debug_cat("Server connection closed.\n")
+         })
+
+         socket <<- ws
+       }
+     ))
+
+
+  error_handler <- function(...) {
+    rlang::entrace(...)
+    trace <- rlang::last_trace()
+    message <- list(message=trace$message, trace=sapply(trace$trace$calls, function(c) { toString(c) }))
+    socket$send(jsonlite::toJSON(message))
+  }
+  options(error = error_handler)
+}
+
---
+++ ui/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    socket_connected: false,
+    last_message: null
+  }
+
+  constructor(props) {
+    super(props);
+    this.socket = new WebSocket('ws://localhost:8080');
+    this.socket.onopen = () => {
+      this.setState({socket_connected: true});
+    }
+    this.socket.onmessage = (event) => {
+      this._onMessage(JSON.parse(event.data));
+    };
+  }
+
+  _onMessage(message) {
+    console.log(message);
+    this.setState({last_message: message});
+  }
+
+  render() {
+    return <div>
+      {!this.state.socket_connected
+      ? <span>Connecting to RStudio session...</span>
+      : <span>{
+        this.state.last_message != null
+        ? <span>{this.state.last_message.message[0]}</span>
+        : <span>No error yet</span>
+      }</span>}
+    </div>;
+  }
+}
+
+export default App;
